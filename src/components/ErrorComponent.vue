ffa4673df0c9b712d0ef3e34dbc3f35708b06520
furkanturgut68 <furkanmehmeturgut@gmail.com>
2023-09-20 16:20:32 +0300

    add error comp

---
+++ src/components/ErrorComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Sayfa Bulunamadı</h1>
+      <router-link to="/login">Anasayfaya Dön</router-link>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style scoped>
+  h1 {
+      align-items: center;
+      justify-content: center;
+      display: flex;
+  }
+  a{
+      align-items: center;
+      justify-content: center;
+      display: flex;
+      font-size: 30px;
+  }
+  
+  </style>
